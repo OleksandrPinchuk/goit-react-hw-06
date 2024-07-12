20f4405e73ff0791ddccd8022830858ea509fec8
OleksandrPinchuk <hohhua@gmail.com>
2024-07-13 00:09:06 +0200

    store

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
 import css from './SearchBox.module.css'
-const SearchBox = ({ searchName, handleSearch }) => {
-    
+import { changeFilter, selectName } from '../../redux/filtersSlice';
+const SearchBox = () => {
+    const dispatch = useDispatch();
+    const filter = useSelector(selectName);
+
+    const handleFilterChange = (event) => {
+    dispatch(changeFilter(event.target.value));
+    };
+
     return (
         <div className={css.box}>
             <label htmlFor='find' className={css.label}>Find contacts by name</label>
-            <input type='text' name='find' id='find' value={searchName} onChange={(e)=>handleSearch(e.target.value)} className={css.field} />
+            <input type='text' name='find' id='find' value={filter} onChange={handleFilterChange} className={css.field} />
         </div>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['contacts'],
+    whitelist: ['items'],
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
